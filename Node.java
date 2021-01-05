bd29cb6d2bc84c795c9fdf535e62fae4cfc8ea08
SHKim10 <skim10@stuy.edu>
2021-01-05 13:27:13 -0500

    Created classes

---
+++ Node.java
@@ -0,0 +1,12 @@
+public class Node{
+ private String data;
+ private Node next, prev;
+
+ //Write get/set methods for all three instance variables.
+ public String getData();
+ public void setData(String data);
+ public String getNext();
+ public void setNext(Node next);
+ public String getPrev();
+ public void setPrev(Node prev);
+}

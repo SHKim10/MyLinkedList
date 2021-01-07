f4535b4d53c813a0c7e1f15561c80759e0003b32
SHKim10 <skim10@stuy.edu>
2021-01-07 09:48:40 -0500

    Implemented first add method

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -9,7 +9,20 @@ public class MyLinkedList{
   return size;
  }
 
- public boolean add(String value);
+ public boolean add(String value){
+   Node a = new Node(value);
+   if (size == 0){
+     start = a;
+     end = a;
+     size++;
+   } else {
+     end.setNext(a);
+     end = a;
+   }
+   size++;
+   return true;
+ }
+
  public boolean add(int index, String value);
  public String get(int index);
  public String set(int index, String value);
